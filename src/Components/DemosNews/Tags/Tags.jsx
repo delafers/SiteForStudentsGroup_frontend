1fdf3011620b9f8a1365e1c4c08dca4592434578
Endektor <v.nikolaev55555@mail.ru>
2020-09-12 20:07:32 +0300

    replaced <a> by <Link> in demosnews tags

--- src/Components/DemosNews/Tags/Tags.jsx
+++ src/Components/DemosNews/Tags/Tags.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Style from './Tags.module.css';
+import {Link} from "react-router-dom";
 
 const Tags = (props) => {
     if (props.tags) {
         return (
             <div className={props.style}>
                 {props.tags.map( tag =>
-                    <a href={'http://localhost:3000/demosnews/?tag=' + tag.id} className={Style.Tag} key={tag.name}>{tag.name}</a>
+                    <Link to={'/demosnews/?tag=' + tag.id}
+                          className={Style.Tag}
+                          key={tag.name}
+                          onClick={() => props.changeTag(tag.id)}
+                    >{tag.name}</Link>
+
+                    // <a href={'http://localhost:3000/demosnews/?tag=' + tag.id} className={Style.Tag} key={tag.name}>{tag.name}</a>
                 )}
             </div>
         );
